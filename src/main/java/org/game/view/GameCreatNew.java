2cde1e949171f5f8aa2f73d01a1aa21cb4e60cc2
Chuan Dong <dongchuan55@gmail.com>
2014-06-24 23:37:38 +0200

    [Update] Update GameCreatNew

---
+++ src/main/java/org/game/view/GameCreatNew.java
@@ -0,0 +1,35 @@
+package org.game.view;
+
+import java.awt.Color;
+import java.util.Random;
+
+import javax.swing.JLabel;
+
+public class GameCreatNew {
+	
+	private GameSetColor ColorController;
+	private int times = 16;									
+	Random random = new Random();
+	
+	public void CreateNew(JLabel[][] matrixGame){
+		int i ,j;
+		boolean flag = false;
+		String str;
+		
+		if(times > 0){
+			while(!flag){
+				i = random.nextInt(4);// Random 0 ~ 3
+				j = random.nextInt(4);
+				str = matrixGame[i][j].getText();
+				
+				if((str.compareTo("") == 0)){
+					matrixGame[i][j].setText("2");
+					ColorController.setColor(matrixGame, i, j, "2");
+				
+					times --;
+					flag = true;	
+				}
+			}
+		}
+	}
+}
