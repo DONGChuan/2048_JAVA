48d5aac124d491a2db3da6550e0b7cc6aac75a48
Chuan Dong <dongchuan55@gmail.com>
2014-07-03 23:14:27 +0200

    [Update] creat CountScore for the bestScore

---
+++ src/main/java/org/game/view/CountScore.java
@@ -0,0 +1,56 @@
+package org.game.view;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.game.model.Score;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class CountScore {
+	
+	private Score score;
+	private final String PATHXML = "src/main/resources/org/game/resources/game.xml";
+	
+	public Score getScore() {
+		return score;
+	}
+
+
+	public void setScore(Score score) {
+		this.score = score;
+	}
+
+	public Score getScoreXML() throws ParserConfigurationException, SAXException, IOException {
+		
+		File fXmlFile = new File(PATHXML);
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(fXmlFile);
+	 
+		NodeList nList = doc.getElementsByTagName("Score");
+	    
+		Score tmp = new Score();
+	    tmp.setScore(Integer.parseInt(nList.item(0).getTextContent()));
+	    
+	    return tmp;
+		
+	}
+	
+    public void setScoreXML(Score score) throws ParserConfigurationException, SAXException, IOException {
+		
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(PATHXML);
+ 
+		Node sc = doc.getElementsByTagName("Score").item(0);
+		sc.setTextContent(String.valueOf(score.getScore()));
+ 
+	}
+}
---
+++ src/test/java/org/game/test/TestCountScore.java
@@ -0,0 +1,38 @@
+package org.game.test;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.game.model.Score;
+import org.game.view.CountScore;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+public class TestCountScore {
+
+	@Test
+	public void testGetScoreXML() throws ParserConfigurationException, SAXException, IOException {
+		
+		// Before test, set score in Game.xml to 1000 
+		CountScore a = new CountScore();
+		a.setScore(a.getScoreXML());
+
+		assertEquals(1000, a.getScore().getScore());
+	}
+	
+	@Test
+	public void testSetScoreXML() throws ParserConfigurationException, SAXException, IOException {
+		
+		CountScore a = new CountScore();
+		Score tmp = new Score();
+		tmp.setScore(2000);
+		
+		a.setScore(a.getScoreXML());
+
+		assertEquals(2000, a.getScore().getScore());
+	}
+
+}
